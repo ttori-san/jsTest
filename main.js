2d2262059bae37ea9d86b63a598c71b1f3ec51b2
ttori-san <akira0901tori@softbank.ne.jp>
2021-03-16 18:45:12 +0900

    up to 28 Object+method,property

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const myMap = new Map();
 
 myMap.set('id',1);
 myMap.set('name','Honda');
-console.log(myMap.get('name'));
+console.log(myMap.get('id'));
 
 const mapKeys = myMap.keys();
 
@@ -16,11 +16,50 @@ const fruits = [//シンタックスシュガー ==new Array();
 fruits.push("みかン");
 
 // コールバック関数
-fruits.forEach(function(fruit){
-  console.log(fruit);
-}
-);
+// fruits.forEach(function(fruit){
+//   console.log(fruit);
+// }
+// );
+fruits.forEach(fruit => console.log(fruit));
 
+
+
+//アロー関数 有名・無名 選べる
+// const firstArrow = () => {console.log('Hello,arrow')};
+const firstArrow = () => console.log('Hello,arrow');
+
+firstArrow();
+
+const getItem = () => {console.log('あろー')};
+getItem();
+
+//Array.filter
+
+const scores = [10,20,30,40,50];
+
+const goodScores = scores.filter(eachScore => eachScore >= 30);
+
+console.log(goodScores);
+
+const Chars = ["a", "b", "c", "d"];
+
+const findChar = Chars.find(eachChar => eachChar === "b");
+console.log(findChar);
+
+const userList = [0,1,2,3,4,5];
+
+const userIdList = userList.map(id => {
+  return `user_${id}`;
+})
+console.log(userIdList);
+console.table(userIdList);
+
+
+const Rolls = ['atacker','braster', 'jammer', 'healer'];
+const OffensiveRolls = Rolls.filter(eachRoll => eachRoll !== 'healer');
+console.log(OffensiveRolls);
+
+// const players  = new Object();・・・下の意味と一緒
 const players = {
   "Honda":{
     "number":4,
@@ -31,6 +70,41 @@ const players = {
     "dominant_foot":'right',
   }
 }
-Object.keys(players).forEach(function(number){
-  console.log(number);
-});
+//取得の際にはオブジェクト・オブジェクトを使う
+console.log(Object.values(players.Honda));
+console.log(Object.keys(players.Honda));
+for ( player of Object.keys(players)){
+  console.log(player);
+}
+
+
+const postalCode ='1234-5678';
+console.log(typeof(postalCode));
+const changedToNum = Number(postalCode);
+console.log(typeof(changedToNum),changedToNum);//numberだがNaN
+
+
+// checkPostalCode:function(){
+  // }以下の書き方と同意
+  const postal ={
+
+  // postalCode : '1234-5678',
+
+  checkPostalCode(string){
+    const replacedPostalCode = string.replace("-","");
+    if (replacedPostalCode.length === 7 ){
+      return true;
+    }else{
+      return false;
+    }
+  }
+}
+console.log(postal.checkPostalCode(postalCode));
+// const checking = function checkPostalCode(string){
+//   const replacedPostalCode = string.replace("-","");
+//   if (replacedPostalCode.length === 7 ){
+//     return true;
+//   }else{
+//     return false;
+//   }
+// }
